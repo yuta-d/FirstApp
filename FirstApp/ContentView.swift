559ed9735c010c9ffc091371e5a2ea7284a82429
yuta-d <yutaro.d.14@gmail.com>
2021-07-28 22:59:57 +0900

    Initial Commit

---
+++ FirstApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by ドイ　ユウタロウ on 2021/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by ドイ　ユウタロウ on 2021/07/28.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
